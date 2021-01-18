3583fc1bc65ec9d5fb4df97a1860c8ec7cf9b764
eduardolm <lodi001@uol.com.br>
2021-01-18 11:03:23 -0300

    Added sort and pagination to list method in controller

--- src/main/java/br/com/alura/forum/controller/TopicController.java
+++ src/main/java/br/com/alura/forum/controller/TopicController.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -31,9 +32,10 @@ public class TopicController {
     @GetMapping
     public Page<TopicDto> list(@RequestParam(required = false) String courseName,
                                @RequestParam int page,
-                               @RequestParam int quantity) {
+                               @RequestParam int quantity,
+                               @RequestParam String orderBy) {
 
-        Pageable pagination = PageRequest.of(page, quantity);
+        Pageable pagination = PageRequest.of(page, quantity, Sort.Direction.ASC, orderBy);
 
         if (courseName == null) {
             Page<Topic> topics = topicService.findAll(pagination);
