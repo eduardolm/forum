256d8eb2b0e7d3051a30146ad2238ea1a5d8c018
eduardolm <lodi001@uol.com.br>
2021-01-19 19:48:48 -0300

    Added Swagger API documentation to the project.

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -59,6 +59,11 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     // Static content configuration (js, css, images, etc.)
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring().antMatchers(
+                "/**.html",
+                "/v2/api-docs",
+                "/webjars/**",
+                "/configuration/**",
+                "/swagger-resources/**");
     }
 }
